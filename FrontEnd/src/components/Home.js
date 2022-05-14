1116fac767089fa57b87a0642d35b047e7eed8ce
TejParmar10 <tejparmar100@gmail.com>
2022-05-14 14:51:34 +0530

    commit

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -28,30 +28,6 @@ const Home = () => {
 			<section className="hero-section">
 				<div className="row">
 					{/* <Jumbotron> */}
-					<div className="col-md-7 col-lg-7">
-						<div className="jumbotron-caption">
-							<h1>Top Influencer Marketing Agency (Platform) India</h1>
-							<p>InfluencerMania is a leading Influencer marketing agency in India which provides the best influencer
-								marketing
-								platform to help brands / visionary marketers leverage social media influencers content in marketing the
-								product (service). Our influencer marketing experts unite brands with top 50,000+ content creators,
-								social
-								media influencers, artists and bloggers with interested viewers (potential customers) for executing
-								remarkable
-								influencer marketing campaigns.</p>
-							{/* <div className="banner-cta">
-              <a onclick="return redirectToCreator()" className="btn btn-info">I am a Creator</a>
-              <a onclick="return redirectToBrand()" className="btn btn-danger">I am a Brand</a>
-            </div> */}
-						</div>
-					</div>
-					<div className="col-md-5 col-lg-5">
-						<div className="jumbotron-slider">
-							<img name="slide" id="slider" className="img-fluid" alt=""/>
-						</div>
-					</div>
-					{/* </Jumbotron> */}
-				</div>
 				<h1 style={{textAlign: "center"}}>Our Collaborations</h1>
 				<div className="App">
 					<Carousel breakPoints={breakPoints} isRTL={false}>
@@ -62,6 +38,7 @@ const Home = () => {
 						<Item><img src={armani} alt={''}></img></Item>
 						<Item><img src={puma} alt={''}></img></Item>
 					</Carousel>
+					</div>
 				</div>
 			</section>
 		</>
